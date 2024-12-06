03bbeae88d6a6d753595c54b1027b5e934dd4957
Joao-viana1 <joao28092003@gmail.com>
2024-12-06 13:07:48 +0000

    atualizando arquivos melhorados relacionados ao grafico de gasolina

--- gasolina.py
+++ gasolina.py
@@ -1,3 +1,11 @@
 sns.set_theme(style="darkgrid")
 
-grafico = sns.lineplot(x="dia", y="venda", palette='pastel', data=gasolina_df)
+
+plt.figure(figsize=(10, 6))
+grafico = sns.lineplot(x="dia", y="venda", data=gasolina_df, color="blue",linewidth=2.5)
+
+plt.title("Vendas de Gasolina por Dia", fontsize=16, weight='bold')
+plt.xlabel("Dia", fontsize=12)
+plt.ylabel("Vendas (Litros)", fontsize=12)
+
+plt.show()
